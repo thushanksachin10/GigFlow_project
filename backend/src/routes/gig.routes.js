98204cc63d797f596dbaa655b3d5ca47d439235a
thushanksachin10 <thushanksachin@gmail.com>
2026-01-12 23:16:58 +0530

    improvements in routes

--- backend/src/routes/gig.routes.js
+++ backend/src/routes/gig.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createGig, getGigs } from "../controllers/gig.controller.js";
+import { createGig, getGigs, getGigById } from "../controllers/gig.controller.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
